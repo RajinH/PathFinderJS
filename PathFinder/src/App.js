c574ad36652507e90e11a0cb6aaddb61714a7150
Rajin Hossain <u7313467@anu.edu.au>
2021-11-17 23:31:36 +1100

    updated start position

--- PathFinder/src/App.js
+++ PathFinder/src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import Node from './components/Node';
-import Grid from './components/Grid';
-import './App.css';
+import logo from "./logo.svg";
+import Node from "./components/Node";
+import Grid from "./components/Grid";
+import "./App.css";
 
 function App() {
   return (
-    <div className={'Wrapper'}>
+    <div className={"Wrapper"}>
       <h1
         style={{
-          textAlign: 'center',
+          textAlign: "center",
         }}
       >
         Pathfinding Visualiser
       </h1>
       <Grid
-        startPos={{ x: 5, y: 5 }}
+        startPos={{ x: 4, y: 5 }}
         endPos={{ x: 10, y: 10 }}
         size={10}
       ></Grid>
