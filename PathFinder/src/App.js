1fc5c1cb9b0786f3a0ac959350fda9462905af54
Rajin Hossain <u7313467@anu.edu.au>
2021-11-20 01:56:53 +1100

    Styling for nodes updated for 3D effect

--- PathFinder/src/App.js
+++ PathFinder/src/App.js
@@ -2,7 +2,10 @@ import logo from "./logo.svg";
 import Node from "./components/Node";
 import Grid from "./components/Grid";
 import "./App.css";
+import React from "react";
 import MainView from "./views/MainView";
+
+const button = React.createContext();
 
 function App() {
   return <MainView></MainView>;
--- PathFinder/src/views/MainView.js
+++ PathFinder/src/views/MainView.js
@@ -5,13 +5,13 @@ import Canvas from "./Canvas";
 function MainView() {
   return (
     <div className={"MainPane"}>
-      <h1>Pathfinding Visualisation</h1>
+      <h1>Pathfinding/Search Visualisation</h1>
       <Canvas></Canvas>
       <div className={"ButtonPane"}>
         <h1>Algorithms to try!</h1>
         <button>A* Algorithm</button>
         <button>Dijkstra's Algorithm</button>
-        <button>Rajin's Algorithm</button>
+        <button>A* (w/ Jump Point Search) Algorithm </button>
       </div>
     </div>
   );
